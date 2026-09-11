dcbdfa5e5f3bb3b8411ee0a01e720d324d330680
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-09-11 02:09:48 -0500

    Update UpCounter.v

--- Modules/Counters/UpCounter.v
+++ Modules/Counters/UpCounter.v
@@ -61,7 +61,7 @@ module UpCounter #(
     input  clk,                     // clock
     input  rst,                     // synchronous reset
     input  count_en,                // enable signal to run counter
-    input  load_en                  // if asserted, load the count register with load_val
+    input  load_en,                 // if asserted, load the count register with load_val
     input  [WIDTH-1:0] load_val,    // value with which to load the counter (starting value; may be non-0 in some cases)
     input  [WIDTH-1:0] target_val,  // value that we are counting up to (target value)
     input  hold_or_loop,            // hold the counter at 0 until reset or keep loading and counting down in a loop (0 : hold, 1 : loop)
